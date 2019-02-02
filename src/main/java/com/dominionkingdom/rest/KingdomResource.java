d5d4f961aacceac3829470544b19ca85775d38de
Jake Nash <jakenash23@gmail.com>
2019-02-02 15:22:59 -0800

    Added boxes query to get entire contents of multiple boxes

--- src/main/java/com/dominionkingdom/rest/KingdomResource.java
+++ src/main/java/com/dominionkingdom/rest/KingdomResource.java
@@ -4,7 +4,10 @@ import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.dynamodbv2.document.DynamoDB;
 import com.amazonaws.services.dynamodbv2.document.ItemCollection;
+import com.amazonaws.services.dynamodbv2.document.QueryOutcome;
 import com.amazonaws.services.dynamodbv2.document.Table;
+import com.amazonaws.services.dynamodbv2.document.spec.QuerySpec;
+import com.amazonaws.services.dynamodbv2.document.utils.ValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,15 +15,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class KingdomResource {
 
-    @GetMapping(path = "/box")
-    public String getBoxContents(@RequestParam String box) {
-        AmazonDynamoDB client = AmazonDynamoDBClientBuilder.defaultClient();
-        DynamoDB dynamoDB = new DynamoDB(client);
-        Table cardTable = dynamoDB.getTable("DominionCards");
-
-        ItemCollection<com.amazonaws.services.dynamodbv2.document.QueryOutcome> items = cardTable.query("Box", box);
+    private String CARD_TABLE = "DominionCards";
+    private AmazonDynamoDB client = AmazonDynamoDBClientBuilder.defaultClient();
+    private DynamoDB dynamoDB = new DynamoDB(client);
+    private Table cardTable = dynamoDB.getTable(CARD_TABLE);
+    
+    @GetMapping(path = "/boxes")
+    public String getMultipleBoxContents(@RequestParam String[] box) {
         StringBuilder output = new StringBuilder();
-        items.forEach(output::append);
+        for (String curr : box) {
+            QuerySpec spec = new QuerySpec()
+                    .withKeyConditionExpression("Box = :v_box")
+                    .withValueMap(new ValueMap()
+                        .withString(":v_box", curr));
+
+            ItemCollection<QueryOutcome> items = cardTable.query(spec);
+            items.forEach(output::append);
+        }
         return output.toString();
     }
 
